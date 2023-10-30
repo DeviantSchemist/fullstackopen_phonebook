4f60282a35190f4dbabae3d66b7862b2a86b43fe
Michael Ngo <michaelngo1337@yahoo.com>
2023-10-30 15:18:46 -0700

    exercise 2.8

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '123-123-6999' }
   ]) 
   const [newName, setNewName] = useState('')
-
-  const handleInput = event => {
-    setNewName(event.target.value);
-  }
+  const [phoneNum, setPhoneNum] = useState('')
 
   const handleSubmit = event => {
     event.preventDefault();
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, phone: phoneNum };
     const personExists = persons.find(person => JSON.stringify(person) === JSON.stringify(newPerson));
     if (!personExists) {
       setPersons(persons.concat(newPerson));
       setNewName('');
+      setPhoneNum('');
       return;
     }
 
@@ -28,14 +26,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleInput} />
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          number: <input value={phoneNum} onChange={(event) => setPhoneNum(event.target.value)} />
         </div>
         <div>
           <button onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} p# {person.phone}</p>)}
     </div>
   )
 }
